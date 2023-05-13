8d18b6dee4ad4187c0c716052a66ffd25e3bbad9
Shuvkant <shuvkantphanait@gmail.com>
2023-05-13 14:49:00 +0545

    changes

--- Guessing_the_num.c
+++
@@ -1,8 +0,0 @@
-/*This is the fun program*/
-#include<stdio.h>
-int main(){
-    int number;
-    printf("Enter the number:");
-    scanf("%d", &number);
-    printf("You have entered %d", number);
-}
--- clearing_doubts.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a;
-    a=30%50;
-    printf("%d", a);
-}
